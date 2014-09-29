f6861055b64f875131b61972939f4e7b6d4da4de
Ethan Lillie <lillieethan@gmail.com>
2014-09-29 08:56:21 -0700

    Initial Commit

---
+++ Chapter8Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter8Swift
+//
+//  Created by Ethan Lillie on 9/29/14.
+//  Copyright (c) 2014 Algorithms. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
